2f92735488372c53fbddd8cecbadb28dec95091e
Felix Lorenzo <37298563+feest19@users.noreply.github.com>
2018-08-21 19:13:25 -0400

    Initial commit

---
+++ multiplicar_dos_numero/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicar_dos_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 50, valor2 = 51, resultado;
+            Console.WriteLine("vamos a multiplicar 50x51");
+            resultado = valor1 * valor2;
+            Console.WriteLine("el resultado de la multiplicacion es: "+resultado);
+            Console.ReadKey();
+        }
+    }
+}
